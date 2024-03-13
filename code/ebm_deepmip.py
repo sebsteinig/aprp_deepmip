dc78f9b79d79459bfb9966d69e70efe925d27590
sebsteinig <seb.steinig@gmail.com>
2024-03-13 22:46:44 +0000

    added EBM-APRP comparison

--- code/ebm_deepmip.py
+++ code/ebm_deepmip.py
@@ -21,13 +21,15 @@ import netCDF4 as nc4
 # Adapted for DeepMIP simulations by Sebastian Steinig (sebastian.steinig@bristol.ac.uk)
 #=================================================================
 
-def ebm_main(dataPaths1, firstMonth1, lastMonth1, dataPaths2, firstMonth2, lastMonth2, out_file):
+def ebm_main(dataPaths1, firstMonth1, lastMonth1, dataPaths2, firstMonth2, lastMonth2, out_file, aprp_file):
 
     #Load files and run calculations for first time period
     dict_input1  = getZonalMeans(dataPaths1, firstMonth1, lastMonth1)
     dict_input2  = getZonalMeans(dataPaths2, firstMonth2, lastMonth2)
+    dict_aprp    = getZonalMeanAPRP(aprp_file, firstMonth1, lastMonth1)
 
-    dict_output  = ebm_partitioning(dict_input1, dict_input2)
+    # print(dict_aprp)
+    dict_output  = ebm_partitioning(dict_input1, dict_input2, dict_aprp)
 
     # output results
     out_nc      = nc4.Dataset(out_file, 'w')
@@ -56,6 +58,10 @@ def ebm_main(dataPaths1, firstMonth1, lastMonth1, dataPaths2, firstMonth2, lastM
     dt_swcre     = out_nc.createVariable('dt_swcre'   , 'f4', ('time','lat'))
     # heat transport convergence
     dt_htc     = out_nc.createVariable('dt_htc'   , 'f4', ('time','lat'))
+    # APRP
+    dt_aprp_alf = out_nc.createVariable('dt_aprp_alf'   , 'f4', ('time','lat'))
+    dt_aprp_cld = out_nc.createVariable('dt_aprp_cld'   , 'f4', ('time','lat'))
+    dt_aprp_clr = out_nc.createVariable('dt_aprp_clr'   , 'f4', ('time','lat'))
 
     lat.units       = 'degrees_north'
     time.units      = 'days since 0001-01-01 00:00:00'
@@ -67,14 +73,17 @@ def ebm_main(dataPaths1, firstMonth1, lastMonth1, dataPaths2, firstMonth2, lastM
     ts2     [:] = dict_input2['ts' ][:]
     dt_gcm  [:] = dict_output['dt_gcm'][:]
     dt_ebm  [:] = dict_output['dt_ebm'][:]
-    # dt_emm  [:] = dict_output['dt_emm'][:]
-    # dt_lwcre[:] = dict_output['dt_lwcre'][:]
+    dt_emm  [:] = dict_output['dt_emm'][:]
+    dt_lwcre[:] = dict_output['dt_lwcre'][:]
     # dt_topo [:] = dict_output['dt_topo'][:]
     # dt_gg   [:] = dict_output['dt_gg'][:]
-    # dt_palb [:] = dict_output['dt_palb'][:]
-    # dt_salb [:] = dict_output['dt_salb'][:]
-    # dt_swcre[:] = dict_output['dt_swcre'][:]
-    # dt_htc  [:] = dict_output['dt_htc'][:]
+    dt_palb [:] = dict_output['dt_palb'][:]
+    dt_salb [:] = dict_output['dt_salb'][:]
+    dt_swcre[:] = dict_output['dt_swcre'][:]
+    dt_htc  [:] = dict_output['dt_htc'][:]
+    dt_aprp_alf[:] = dict_output['dt_aprp_alf'][:]
+    dt_aprp_cld[:] = dict_output['dt_aprp_cld'][:]
+    dt_aprp_clr[:] = dict_output['dt_aprp_clr'][:]
     time    [:] = [15.5, 45, 74.5, 105, 135.5, 166, 196.5, 227.5, 258, 288.5, 319, 349.5]
     lat     [:] = dict_input1['lat'][:]
 
@@ -95,7 +104,7 @@ def find_longitude_axis(variable):
             return i
     raise ValueError("Longitude dimension not found in variable dimensions")
 
-# read data
+# read input data
 def getZonalMeans(dataPaths, firstMonth, lastMonth):
     datasets = {var: nc4.Dataset(dataPaths[var]) for var in dataPaths}
 
@@ -123,41 +132,115 @@ def getZonalMeans(dataPaths, firstMonth, lastMonth):
 
     return zonal_means
 
-def ebm_partitioning(input_ctrl, input_sens):
+
+# read APRP results
+def getZonalMeanAPRP(aprp_file, firstMonth, lastMonth):
+    dataset = nc4.Dataset(aprp_file)
+
+    lat = lon = None
+    for var in dataset.variables:
+        if var in ['lat', 'latitude']:
+            lat = dataset.variables[var][:]
+        elif var in ['lon', 'longitude']:
+            lon = dataset.variables[var][:]
+        elif var in ['t', 'time', 'month', 'time_counter']:
+            time = dataset.variables[var][:]
+
+    var_list = ["alf", "cld", "clr"]
+    zonal_means = {}
+    for var in var_list:
+        variable_data = dataset.variables[var]
+        # Replace masked values with 0
+        variable_data_filled = dataset.variables[var][firstMonth:lastMonth+1,:,:].filled(0)
+        lon_axis = find_longitude_axis(variable_data)
+        zonal_mean = np.mean(variable_data_filled, axis=lon_axis)
+        zonal_means[f"{var}"] = zonal_mean
+
+    zonal_means['lat'] = lat
+    zonal_means['lon'] = lon
+    zonal_means['time'] = time
+
+    return zonal_means
+
+def ebm_partitioning(input_ctrl, input_sens, dict_aprp):
 
     # EBM for control experiment
-    palb_ctrl = np.where(input_ctrl['rsdt'] != 0, input_ctrl['rsut'] / input_ctrl['rsdt'], 0)  # Avoid division by zero
-    # palb_ctrl       = input_ctrl['rsut'] / input_ctrl['rsdt'] # Planetary albedo
+    # all-sky fluxes
+    palb_ctrl       = np.where(input_ctrl['rsdt'] != 0, input_ctrl['rsut'] / input_ctrl['rsdt'], 0)  # Avoid division by zero
     emm_ctrl        = input_ctrl['rlut'] / input_ctrl['rlus'] # Emissivity
     heatt_ctrl      = - ( input_ctrl['rsdt'] - input_ctrl['rsut'] - input_ctrl['rlut'] ) # Heat transport convergence
     insolation_ctrl = input_ctrl['rsdt']
-    tsurf_ctrl  = calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+    tsurf_ctrl      = calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+    # clear-sky fluxes
+    palb_ctrl_cs    = np.where(input_ctrl['rsdt'] != 0, input_ctrl['rsutcs'] / input_ctrl['rsdt'], 0) 
+    emm_ctrl_cs     = input_ctrl['rlutcs'] / input_ctrl['rlus']
+    heatt_ctrl_cs   = - ( input_ctrl['rsdt'] - input_ctrl['rsutcs'] - input_ctrl['rlutcs'] )
 
     # EBM for sensitivity experiment
-    palb_sens       = input_sens['rsut'] / input_sens['rsdt'] # Planetary albedo
+    # all-sky fluxes
+    palb_sens       = np.where(input_sens['rsdt'] != 0, input_sens['rsut'] / input_sens['rsdt'], 0)  # Avoid division by zero
     emm_sens        = input_sens['rlut'] / input_sens['rlus'] # Emissivity
-    heatt_sens      = - ( input_sens['rsdt'] - input_sens['rsut'] ) - input_sens['rlut'] # Heat transport convergence
+    heatt_sens      = - ( input_sens['rsdt'] - input_sens['rsut'] - input_sens['rlut'] ) # Heat transport convergence
     insolation_sens = input_sens['rsdt']
     tsurf_sens  = calc_ebm_surface_temp(palb_sens, emm_sens, heatt_sens, insolation_sens)
+    # clear-sky fluxes
+    palb_sens_cs       = np.where(input_sens['rsdt'] != 0, input_sens['rsutcs'] / input_sens['rsdt'], 0)
+    emm_sens_cs        = input_sens['rlutcs'] / input_sens['rlus']
+    heatt_sens_cs      = - ( input_sens['rsdt'] - input_sens['rsutcs'] - input_sens['rlutcs'] )
+
+    # perturb individual parameters to get their temperature response
+    dt_emm = calc_ebm_surface_temp(palb_ctrl, emm_sens, heatt_sens, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+    dt_emm_cs = calc_ebm_surface_temp(palb_ctrl_cs, emm_sens_cs, heatt_sens_cs, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl_cs, emm_ctrl_cs, heatt_ctrl_cs, insolation_ctrl)
+    dt_lwcre = dt_emm - dt_emm_cs
+
+    dt_palb = calc_ebm_surface_temp(palb_sens, emm_ctrl, heatt_ctrl, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+    dt_palb_cs = calc_ebm_surface_temp(palb_sens_cs, emm_ctrl_cs, heatt_ctrl_cs, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl_cs, emm_ctrl_cs, heatt_ctrl_cs, insolation_ctrl)
+ 
+    # Lunt et al. (2021): surface albedo term is calculated via palb + (salb_sens - salb_ctrl)
+    salb_ctrl = np.where(input_ctrl['rsds'] >= 0.1, input_ctrl['rsus'] / input_ctrl['rsds'], 0)  # Avoid division by zero 
+    salb_sens = np.where(input_sens['rsds'] >= 0.1, input_sens['rsus'] / input_sens['rsds'], 0)  # Avoid division by zero 
+    dt_salb = calc_ebm_surface_temp(palb_ctrl + ( salb_sens - salb_ctrl ), emm_ctrl, heatt_ctrl, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+
+    # other possibility: surface albedo term is just change in clear-sky albedo
+    # dt_salb = dt_palb_cs
+
+    dt_swcre = dt_palb - dt_salb
+# 
+    dt_htc = calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_sens, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+
+    # calculate temperature response due to planetary albedo changes from APRP
+    # get plaenatry APRP albedo changes by dividing radiative effects (in W/m^2) by insolation (in W/m^2)
+    # basically reversing lines 282-292 in aprp_deepmip.py
+    palb_aprp_alf = np.where(input_sens['rsdt'] != 0, dict_aprp['alf'] / input_sens['rsdt'] * ( -1.0 ), 0)  # Avoid division by zero
+    palb_aprp_cld = np.where(input_sens['rsdt'] != 0, dict_aprp['cld'] / input_sens['rsdt'] * ( -1.0 ), 0)  # Avoid division by zero
+    palb_aprp_clr = np.where(input_sens['rsdt'] != 0, dict_aprp['clr'] / input_sens['rsdt'] * ( -1.0 ), 0)  # Avoid division by zero
+    # calculate temperature response due to planetary albedo changes from APRP
+    dt_aprp_alf = calc_ebm_surface_temp(palb_ctrl + palb_aprp_alf, emm_ctrl, heatt_ctrl, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+    dt_aprp_cld = calc_ebm_surface_temp(palb_ctrl + palb_aprp_cld, emm_ctrl, heatt_ctrl, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
+    dt_aprp_clr = calc_ebm_surface_temp(palb_ctrl + palb_aprp_clr, emm_ctrl, heatt_ctrl, insolation_ctrl) - calc_ebm_surface_temp(palb_ctrl, emm_ctrl, heatt_ctrl, insolation_ctrl)
 
     #Save the relevant variables to a dictionary for writing to netCDF
     dict_out = dict()
-    dict_out['dt_gcm'] = palb_ctrl
-    dict_out['dt_ebm'] = tsurf_ctrl
-    # dict_out['dt_emm'] = dt_emm
-    # dict_out['dt_lwcre'] = dt_lwcre
+    dict_out['dt_gcm'] = input_sens['ts'] - input_ctrl['ts']
+    dict_out['dt_ebm'] = tsurf_sens - tsurf_ctrl
+    dict_out['dt_emm'] = dt_emm
+    dict_out['dt_lwcre'] = dt_lwcre
     # dict_out['dt_topo'] = dt_topo
     # dict_out['dt_gg'] = dt_gg
-    # dict_out['dt_palb'] = dt_palb
-    # dict_out['dt_salb'] = dt_salb
-    # dict_out['dt_swcre'] = dt_swcre
-    # dict_out['dt_htd'] = dt_htd
+    dict_out['dt_palb'] = dt_palb
+    # dict_out['dt_salb'] = dt_palb_cs
+    dict_out['dt_salb'] = dt_salb
+
+    dict_out['dt_swcre'] = dt_swcre
+    dict_out['dt_htc'] = dt_htc
+    dict_out['dt_aprp_alf'] = dt_aprp_alf
+    dict_out['dt_aprp_cld'] = dt_aprp_cld
+    dict_out['dt_aprp_clr'] = dt_aprp_clr
 
     return dict_out
 
 def calc_ebm_surface_temp(palb, emm, heat, insolation):
     sigma       = 5.670367e-8 # Stefan-Boltzmann constant
     tsurf       = ( 1. / emm / sigma * ( insolation * ( 1. - palb ) + heat ) ) ** 0.25
-    # tsurf       = ( 1. / emm / sigma * ( insolation * ( 1. - palb ) + heat ) ) 
     return tsurf
 
--- code/run_ebm.deepmip.py
+++ code/run_ebm.deepmip.py
@@ -6,6 +6,7 @@ import subprocess
 
 data_dir    = '/Volumes/WD_Elements/DeepMIP_database/ceda/deepmip-eocene-p1/'
 out_dir = '/Users/wb19586/Documents/coding_github/aprp_deepmip/ebm_output_data/deepmip/'
+out_dir_aprp = '/Users/wb19586/Documents/coding_github/aprp_deepmip/aprp_output_data/deepmip/'
 
 months      = [0, 11]
 
@@ -30,7 +31,9 @@ for model in deepmip_dict.keys():
                        'rlut'   : prefix1 + 'rlut_'  + posfix1,
                        'rlus'   : prefix1 + 'rlus_'  + posfix1,
                        'rsutcs'   : prefix1 + 'rsutcs_'  + posfix1,
-                       'rlutcs'   : prefix1 + 'rlutcs_'  + posfix1}
+                       'rlutcs'   : prefix1 + 'rlutcs_'  + posfix1,
+                       'rsds'   : prefix1 + 'rsds_'  + posfix1,
+                       'rsus'   : prefix1 + 'rsus_'  + posfix1}
         sens_files  = {'ts'    : prefix2 + 'ts_'   + posfix2,
                        'ps'    : prefix2 + 'ps_'   + posfix2,
                        'rsut'   : prefix2 + 'rsut_'  + posfix2,
@@ -38,7 +41,12 @@ for model in deepmip_dict.keys():
                        'rlut'   : prefix2 + 'rlut_'  + posfix2,
                        'rlus'   : prefix2 + 'rlus_'  + posfix2,
                        'rsutcs'   : prefix2 + 'rsutcs_'  + posfix2,
-                       'rlutcs'   : prefix2 + 'rlutcs_'  + posfix2}
+                       'rlutcs'   : prefix2 + 'rlutcs_'  + posfix2,
+                       'rsds'   : prefix2 + 'rsds_'  + posfix2,
+                       'rsus'   : prefix2 + 'rsus_'  + posfix2}
         
         out_file    = out_dir + model + '.' + deepmip_dict[model]['contr'][i] +'.to.' + deepmip_dict[model]['sensi'][i] + '.ebm.nc'
-        ebm.ebm_main(ctrl_files, months[0], months[1], sens_files, months[0], months[1], out_file)
+        # pass APRP output to EBM function to calculate temperature response due to APRP components
+        aprp_file    = out_dir_aprp + model + '.' + deepmip_dict[model]['contr'][i] +'.to.' + deepmip_dict[model]['sensi'][i] + '.aprp.nc'
+
+        ebm.ebm_main(ctrl_files, months[0], months[1], sens_files, months[0], months[1], out_file, aprp_file)
